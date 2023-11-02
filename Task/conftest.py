08dbea6cd014c5a5c06146818e8c14685208d6d4
Khamatshin Ayrat Mac <airatko4@gmail.com>
2023-11-03 00:12:17 +0500

    add files first

---
+++ Task/conftest.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.chrome.options import Options
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose the browser language")
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    browser_name = request.config.getoption("browser_name")
+
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Firefox(options=options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
+    yield browser
+    browser.quit()
