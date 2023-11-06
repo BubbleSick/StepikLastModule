e688af711e1527b8ec59bfcae67fe6ce50c813a3
Khamatshin Ayrat Mac <airatko4@gmail.com>
2023-11-06 22:54:38 +0500

    Реализация LoginPage

--- Task/pages/login_page.py
+++ Task/pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -8,13 +9,11 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        current_url = self.browser.current_url # реализуйте проверку на корректный url адрес
+        assert "login" in current_url, f"Expected 'login' in URL, but got {current_url}"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"# реализуйте проверку, что есть форма логина
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented" # реализуйте проверку, что есть форма регистрации на странице
