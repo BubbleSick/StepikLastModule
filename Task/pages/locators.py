12e07519afd13c85e18784f766d8f1d7b0104a09
Ayrat Khamatshin PC <airatko4@gmail.com>
2023-11-07 21:05:38 +0500

    started writing add_to_basket test

--- Task/pages/locators.py
+++ Task/pages/locators.py
@@ -7,3 +7,11 @@ class MainPageLocators():
 class LoginPageLocators():
    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    BUSKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    SUCCESS_MESSAGE =
+    PRODUCT_NAME = (By.XPATH, "//ul/li[5]/text()")
+    BASKET_TOTAL =
+    PRODUCT_PRICE =
+
---
+++ Task/pages/product_page.py
@@ -0,0 +1,18 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        busket = self.browser.find_element(*ProductPageLocators.BUSKET_BUTTON)
+        busket.click()
+
+    def should_be_success_message(self):
+        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is not presented"
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        success_message = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
+        assert product_name in success_message, "Product name is not in the success message"
+
+    def should_be_basket_total(self):
+        assert self.is_element_present(*ProductPageLocators.BASKET_TOTAL), "Basket total is not presented"
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
+        assert product_price in basket_total, "Product price is not in the basket total"
